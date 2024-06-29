05d80cfdf1cb2ffa6098ec1b4979b9ddb724bc05
Randall Carbonel <rcarboneljr@gmail.com>
2024-06-29 15:19:05 -0700

    improve code readability: Separated calculations and output.

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 
 public class ChangeCalculator {
     public static void main(String[] args) {
+
         // Print out the name of the program followed by a blank line
         System.out.println("Welcome to the Change Calculator");
         System.out.println();
@@ -23,35 +24,30 @@ public class ChangeCalculator {
         String choice = "y";
 
         // Set up the while loop so the program runs until the user exits
-
         while (choice.equalsIgnoreCase("y")) {
+
             // Get user input for the number of cents (0-99) to be calculated for
             System.out.print("Enter number of cents (0-99): ");
             int cents = sc.nextInt();
+            sc.nextLine(); // Clear the newline character
 
-            // Clear the newline character
-            sc.nextLine();
-
-            // Calculate and print the number of quarters
-            int quarters = cents/25;
-            System.out.println("Quarters: " + quarters);
-
+            // Calculations
+            int quarters = cents/25; // Quarters
             cents %= 25;
 
-            // Calculate and print the number of dimes
-            int dimes = cents/10;
-            System.out.println("Dimes: " + dimes);
-
+            int dimes = cents/10; // Dimes
             cents %= 10;
 
-            // Calculate and print the number of nickels
-            int nickels = cents/5;
-            System.out.println("Nickels: " + nickels);
-
+            int nickels = cents/5; // Nickels
             cents %= 5;
 
-            // Print the remaining number cents as pennies
-            System.out.println("Pennies: " + cents);
+            int pennies = cents; // Pennies
+
+            // Print the results
+            System.out.println("Quarters: " + quarters); // Quarters
+            System.out.println("Dimes: " + dimes); // Dimes
+            System.out.println("Nickels: " + nickels); // Nickels
+            System.out.println("Pennies: " + pennies); // Pennies
 
             // Ask the user if they would like to continue
             System.out.print("Continue? (y/n): ");
